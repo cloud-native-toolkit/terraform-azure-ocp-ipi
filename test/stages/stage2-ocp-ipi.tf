9b95087540be1af575559ffbc6996e715df46517
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-05-23 09:19:03 +1000

    Added pull secret to gh workflows
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- test/stages/stage2-ocp-ipi.tf
+++ test/stages/stage2-ocp-ipi.tf
@@ -1,7 +1,7 @@
 module "azure-ocp-ipi" {
   source = "./module"
 
-  name_prefix = var.name_prefix
+  name_prefix = "ipi-test"
   domain_resource_group_name = "ocp-ipi-rg"  
   region = var.region
   subscription_id = var.subscription_id
@@ -10,4 +10,5 @@ module "azure-ocp-ipi" {
   tenant_id = var.tenant_id
   pull_secret = var.pull_secret
   base_domain = "clusters.azure.ibm-software-everywhere.dev"
+  openshift_version = "4.10.11"
 }
--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -1,23 +1,8 @@
 # Resource Group Variables
-variable "resource_group_name" {
-  type        = string
-  description = "Resource group to be created for OCP."
-}
-
-variable "nw_resource_group_name" {
-  type        = string
-  description = "Network resource group to be created."
-}
 
 variable "region" {
   type        = string
   description = "Region/location to deploy into."
-}
-
-variable "name_prefix" {
-  type        = string
-  description = "Prefix name that should be used for the cluster and services. If not provided then resource_group_name will be used"
-  default     = "ocp-test"
 }
 
 variable "enabled" {}
@@ -29,3 +14,5 @@ variable "client_id" {}
 variable "client_secret" {}
 
 variable "tenant_id" {}
+
+variable "pull_secret" {}
