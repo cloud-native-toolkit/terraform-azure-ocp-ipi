2e77057eb0525964e4212b0383de3279b5a3028a
Rich Ehrhardt <51330294+rich-ehrhardt@users.noreply.github.com>
2022-05-23 15:01:59 +1000

    Added outputs for cert module (#3)
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,55 @@
-#output "myoutput" {
-#  description = "Description of my output"
-#  value       = "value"
-#  depends_on  = [<some resource>]
-#}
+output "id" {
+  value       = data.external.oc_login.result.clusterID
+  description = "ID of the cluster"
+}
 
-// temporary output
+output "name" {
+  value       = local.cluster_name
+  description = "Name of the cluster."
+}
+
+output "resource_group_name" {
+  value       = data.external.oc_info.result.infraID
+  description = "Name of the resource group containing the cluster"
+  depends_on  = [
+    data.external.oc_info
+  ]
+}
+
+output "region" {
+  value       = var.region
+  description = "Azure location containing the cluster"
+  depends_on  = [
+    data.external.oc_info
+  ]
+}
+
+output "config_file_path" {
+  value       = "${local.install_path}/auth/kubeconfig"
+  description = "Path to the config file for the cluster."
+  depends_on  = [
+    data.external.oc_login
+  ]
+}
+
 output "consoleURL" {
-    value = data.external.oc_info.result.consoleURL
+  value       = data.external.oc_info.result.consoleURL
+  description = "URL for the cluster console"
 }
 
-output "kubeadminUsername" {
-    value = data.external.oc_info.result.kubeadminUsername
+output "username" {
+  value       = data.external.oc_info.result.kubeadminUsername
+  description = "kubeadmin username for the cluster URL"
 }
 
-output "kubeadminPassword" {
-    value = data.external.oc_info.result.kubeadminPassword
-    # sensitive = true
+output "password" {
+  value       = data.external.oc_info.result.kubeadminPassword
+  description = "kubeadmin password for the cluster URL"
+  sensitive   = true
 }
 
-output "clusterID" {
-  value = data.external.oc_login.result.clusterID
+output "bin_dir" {
+  value       = local.binary_path
+  description = "Path to the client binaries"
 }
 
-output "infraID" {
-  value = data.external.oc_info.result.infraID
-}
