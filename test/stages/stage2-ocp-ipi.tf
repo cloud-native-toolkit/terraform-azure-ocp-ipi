e74559fe92a88139c9be508b3979c18b8f849edd
Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-06-07 17:08:37 +1000

    Updated workflow to use new subscription
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- test/stages/stage2-ocp-ipi.tf
+++ test/stages/stage2-ocp-ipi.tf
@@ -2,13 +2,13 @@ module "azure-ocp-ipi" {
   source = "./module"
 
   name_prefix = "ipi-test"
-  domain_resource_group_name = "ocp-ipi-rg"  
+  domain_resource_group_name = "ocp-domain"  
   region = var.region
   subscription_id = var.subscription_id
   client_id = var.client_id
   client_secret = var.client_secret
   tenant_id = var.tenant_id
   pull_secret = var.pull_secret
-  base_domain = "clusters.azure.ibm-software-everywhere.dev"
+  base_domain = "ocp-azure.ibm-software-everywhere.dev"
   openshift_version = "4.10.11"
 }
