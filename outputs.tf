5274ebda72ee5f0e55b5d54137161fe180b5d94e
Rich Ehrhardt <51330294+rich-ehrhardt@users.noreply.github.com>
2022-06-08 11:51:04 +1000

    4 add platform object output variable (#10)
    * Moved to cli module and added outputs
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Updated workflow to use new subscription
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Added troubleshooting to readme
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Adjusted yq cli in oc-login
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Modified for yq4 v4.16 in cli-tools image
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -37,22 +37,49 @@ output "consoleURL" {
   description = "URL for the cluster console"
 }
 
+output "server_url" {
+  value       = data.external.oc_login.result.serverURL
+  description = "The url used to connect to the api of the cluster."
+}
+
 output "username" {
   value       = data.external.oc_info.result.kubeadminUsername
-  description = "kubeadmin username for the cluster URL"
+  description = "kubeadmin username for the cluster"
 }
 
 output "password" {
   value       = data.external.oc_info.result.kubeadminPassword
-  description = "kubeadmin password for the cluster URL"
+  description = "kubeadmin password for the cluster"
   sensitive   = true
+}
+
+output "token" {
+  description = "The admin user token used to generate the cluster"
+  value = ""
+  sensitive = true
 }
 
 output "bin_dir" {
   value       = local.binary_path
   description = "Path to the client binaries"
+}
+
+output "platform" {
+  value = {
+    id         = data.external.oc_login.result.clusterID
+    kubeconfig = "${local.install_path}/auth/kubeconfig"
+    server_url = data.external.oc_login.result.serverURL
+    type       = local.cluster_type
+    type_code  = local.cluster_type_code
+    version    = local.cluster_version
+    ingress    = data.external.oc_info.result.consoleURL
+    tls_secret = data.external.oc_login.result.serverToken
+  }
+  sensitive = true
+  description = "Configuration values for the created cluster platform"
   depends_on = [
-    null_resource.binary_download
+    data.external.oc_login,
+    data.external.oc_info
   ]
 }
 
