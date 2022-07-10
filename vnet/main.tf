c4c82774869b74b094da6d8dbbf82f43676028a6
rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-07-11 09:31:38 +1000

    Added load balancers
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

--- vnet/main.tf
+++ vnet/main.tf
@@ -97,3 +97,93 @@ resource "azurerm_network_security_rule" "api_inbound" {
   resource_group_name         = module.ocp_resource_group.name
   network_security_group_name = azurerm_network_security_group.cluster.name
 }
+
+// Load balancers
+
+module "internal_lb" {
+    source = "github.com/cloud-native-toolkit/terraform-azure-lb"
+
+    name_prefix         = "${var.name_prefix}-${var.cluster_id}-internal-lb"
+    resource_group_name = module.ocp_resource_group.name
+    region              = var.region
+    public              = false
+    subnet_id           = data.azurerm_subnet.master_subnet.id
+    lb_sku              = "Standard"
+    use_ipv4            = true
+    use_ipv6            = false
+
+    lb_rules            = [{
+        name = "${var.name_prefix}-api-internal-rule"
+        protocol = "Tcp"
+        probe_name = "${var.name_prefix}-api-internal-probe"
+        frontend_port = 6443
+        backend_port = 6443
+        idle_timeout = 30
+        load_distribution = "Default"
+        enable_floating_point = false
+    },
+    {
+        name = "${var.name_prefix}-sint-rule"
+        protocol = "Tcp"
+        probe_name = "${var.name_prefix}-sint-probe"
+        frontend_port = 22623
+        backend_port = 22623
+        idle_timeout = 30
+        load_distribution = "Default"
+        enable_floating_point = false        
+    }]
+
+    lb_probes = [{
+        name = "${var.name_prefix}-api-internal-probe"
+        interval = 5
+        no_probes = 2
+        port = 6443
+        request_path = "/readyz"
+        protocol = "Https"
+    },
+    {
+        name = "${var.name_prefix}-sint-probe"
+        interval = 5
+        no_probes = 2
+        port = 22623
+        request_path = "/healthz"
+        protocol = "Https"
+    }]
+}
+
+module "public_lb" {
+    source = "github.com/cloud-native-toolkit/terraform-azure-lb"
+
+    name_prefix             = "${var.name_prefix}-${var.cluster_id}-public-lb"
+    resource_group_name     = module.ocp_resource_group.name
+    region                  = var.region
+    public                  = true
+    lb_sku                  = "Standard"
+    public_ip_sku           = "Standard"
+    public_ip_allocation    = "Static"
+    outbound_rule           = false    
+    use_ipv4                = true
+    use_ipv6                = false
+    dns_label               = "${var.name_prefix}-aro"
+    create_fqdn             = true 
+
+    lb_rules = [{
+        name = "${var.name_prefix}-api-external-rule"
+        protocol = "Tcp"
+        probe_name = "${var.name_prefix}-api-external-probe"
+        frontend_port = 6443
+        backend_port = 6443
+        idle_timeout = 30
+        load_distribution = "Default"
+        enable_floating_point = false 
+    }]
+
+    lb_probes = [{
+        name = "${var.name_prefix}-api-external-probe"
+        interval = 5
+        no_probes = 2
+        port = 6443
+        request_path = "/readyz"
+        protocol = "Https"        
+    }]
+}
