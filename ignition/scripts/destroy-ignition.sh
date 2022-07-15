d871442e8b02d1981fde890e79ac66501d0db103
rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-07-15 17:21:13 +1000

    Script fix and permission change
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

--- ignition/scripts/destroy-ignition.sh
+++ ignition/scripts/destroy-ignition.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 if [ -e ${INSTALL_PATH}/bootstrap.ign ] ; then
-    rm ${INSTALL_PATH/bootstrap.ign}
+    rm ${INSTALL_PATH}/bootstrap.ign
 fi
 
 if [ -e ${INSTALL_PATH}/master.ign ] ; then
-    rm ${INSTALL_PATH/master.ign}
+    rm ${INSTALL_PATH}/master.ign
 fi
 
 if [ -e ${INSTALL_PATH}/worker.ign ] ; then
-    rm ${INSTALL_PATH/worker.ign}
+    rm ${INSTALL_PATH}/worker.ign
 fi
